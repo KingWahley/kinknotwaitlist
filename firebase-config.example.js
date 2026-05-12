fe04fd04f5e94f02d3b74ea5df350869033026de
KingWahley <kingwahley2@gmail.com>
2026-05-12 15:19:56 +0100

    updates

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "your_firebase_api_key",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.firebasestorage.app",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id",
+  measurementId: "your_measurement_id"
+};
