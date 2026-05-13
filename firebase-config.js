8341a20a43652dd40a117069e820d42ac417455d
KingWahley <kingwahley2@gmail.com>
2026-05-13 15:18:02 +0100

    Fix non-blocking JS and update gitignore

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyB4caE0iaWLiKN8GXCYMWFF4K-bt0kGotQ",
+  authDomain: "saasproject-b922d.firebaseapp.com",
+  projectId: "saasproject-b922d",
+  storageBucket: "saasproject-b922d.firebasestorage.app",
+  messagingSenderId: "192171597664",
+  appId: "1:192171597664:web:b9bec0e1e85959bf7d8805",
+  measurementId: "G-B6T5H09VZY"
+};
